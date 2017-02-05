8183adab4a467b721ba81c8e24fea35eac31ae7d
Lawson Nerenberg <dartellis@gmail.com>
2017-02-05 10:47:21 -0700

    Saved it

---
+++ body.js
@@ -0,0 +1,73 @@
+function Body(_x,_y,_r){
+  this.location = {x:_x, y:_y};
+  this.velocity = {x:0, y:0};
+  this.accelleration = {x:0, y:0};
+
+  this.force = {x:0, y:0};
+  this.forces = [];
+  
+  this.totalForce = {x:0, y:0};
+
+  this.r = _r;
+
+  this.G = 9.8;
+
+  this.mass = _r;
+  this.terminalVelocity = 100;
+
+  this.calculateAttraction = function(bodies,n){
+    this.totalForce.x = 0;
+    this.totalForce.y = 0;
+    
+    for(var i = 0; i < bodies.length; i++){
+      if(i!=n){
+        var force = {x:0, y:0};
+  
+        force.x = bodies[i].location.x - this.location.x;
+        force.y = bodies[i].location.y - this.location.y;
+  
+        var distance = Math.sqrt(Math.pow(force.x,2) + Math.pow(force.y,2));
+  
+        force.x = force.x/ distance;
+        force.y = force.y/ distance;
+  
+        var strength = (this.G * this.mass * bodies[i].mass) / (Math.pow(distance,2));
+        
+        force.x *= strength;
+        force.y *= strength;
+      
+        this.totalForce.x += force.x;
+        this.totalForce.y += force.y;
+      }
+    }
+    var totalForce = this.totalForce;
+    return totalForce;
+  }
+
+  this.update = function(){
+    this.accelleration.x = this.totalForce.x / this.mass;
+    this.accelleration.y = this.totalForce.y / this.mass;
+
+    this.velocity.x += this.accelleration.x;
+    this.velocity.y += this.accelleration.y;
+
+    this.location.x += this.velocity.x;
+    this.location.y += this.velocity.y;
+  };
+
+  this.display = function(){
+    ellipse(this.location.x, this.location.y, this.r);
+    fill(10);
+  };
+   
+  this.displayVector = function(bodies,vectorMag){
+    this.calculateAttraction(bodies);
+   
+    var forceMag = Math.sqrt(Math.pow(this.totalForce.x,2) + Math.pow(this.totalForce.y,2));
+  
+    var normForce = {x:(this.totalForce.x/this.mass)/forceMag, y:(this.totalForce.y/this.mass)/forceMag};
+    
+    line(this.location.x,this.location.y,this.location.x + normForce.x * vectorMag,this.location.y + normForce.y * vectorMag);
+    stroke(200,10,10);
+  };
+}
---
+++ homepage.js
@@ -0,0 +1,45 @@
+var planets = [];
+var vectors = [];
+
+var fieldSize = {x:20, y:20};
+
+var canvasSize = {x:200, y:200};
+
+function setup() {
+  frameRate(30);
+  canvasSize = {x:displayWidth/3, y:windowHeight};
+  var canvas = createCanvas(canvasSize.x, canvasSize.y);
+  canvas.parent("canvas");
+  
+  background(0);
+  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-175),50));
+  planets.push(new Body(int(displayWidth/3-(displayWidth/3/2)),int(windowHeight-175)+100,20));
+  planets[1].velocity.x = 2;
+  for(var i = 0; i < fieldSize.x; i++){
+    vectors.push([]);
+    for(var j = 0; j < fieldSize.y; j++){
+      vectors[i].push(new Body(i * int(canvasSize.x / fieldSize.x) + 10,j * int(canvasSize.y / fieldSize.y) + int((canvasSize.y / fieldSize.y)/2),5));
+    }
+  }
+}
+
+function draw() {
+  background(250, 250, 250); 
+  for(var i = 0; i < fieldSize.x; i++){
+    for(var j = 0; j < fieldSize.y; j++){
+      vectors[i][j].displayVector(planets,30);
+      //vectors[i][j].display();
+    }
+  }
+  planets[1].calculateAttraction(planets,1)
+  planets[1].update();
+  planets[1].display();
+  planets[0].display();
+}
+
+
+function keyPressed(){
+  if(keyCode === 32){
+    console.log(planets[0].calculateAttraction(planets,0));
+  }
+}
