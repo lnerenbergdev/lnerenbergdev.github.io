402e06efcaa32e465b07d783eb11ea94ab9b29ae
lnerenberg <dartellis@gmail.com>
2019-01-02 17:31:19 -0700

    commit

--- homepage.js
+++ homepage.js
@@ -17,6 +17,7 @@ function setup() {
   background(0);
   planets.push(new Body(int(windowWidth/2),int(windowHeight-175),50));
   planets.push(new Body(int(windowWidth/2),int(windowHeight-175)+100,20));
+  planets.push(new Body(int(windowWidth/2),int(windowHeight-175)+200,20));
   planets[1].velocity.x = 2;
   for(var i = 0; i < fieldSize.x; i++){
     vectors.push([]);
@@ -33,7 +34,9 @@ function draw() {
     canvasSize = {x:windowWidth, y:windowHeight};
     planets[0] = new Body(int(windowWidth/2),int(windowHeight-175),50);
     planets[1] = new Body(int(windowWidth/2),int(windowHeight-175)+100,20);
+    planets[1] = new Body(int(windowWidth/2),int(windowHeight-175)+200,20);
     planets[1].velocity.x = 2;
+    planets[2].velocity.x = 3;
     vectors = [];
     for(var i = 0; i < fieldSize.x; i++){
       vectors.push([]);
@@ -50,9 +53,13 @@ function draw() {
   }
   
   planets[1].calculateAttraction(planets,1)
+  planets[2].calculateAttraction(planets,1)
   planets[1].update();
+  planets[2].update();
+  planets[2].display();
   planets[1].display();
   planets[0].display();
+
 
   prevWindowSize.x = windowWidth;
   prevWindowSize.y = windowHeight;
